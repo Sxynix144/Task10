d43929a25c9447809c250765f7cfe70fa53cd0b1
Allen Cosicol <a.cosicol.558318@umindanao.edu.ph>
2026-03-03 15:15:33 +0800

    Update select.php

--- select.php
+++ select.php
@@ -4,4 +4,12 @@ require 'config.php';
 $stmt = $pdo->query("SELECT * FROM users");
 $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$stmtOrders = $pdo->query("
+    SELECT o.orders_id, o.user_id, o.product, o.amount, u.name, u.email 
+    FROM orders o 
+    JOIN users u ON o.user_id = u.user_id
+    ORDER BY o.orders_id DESC
+");
+
 ?>
+
